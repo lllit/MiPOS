41d9dbf72830f97df0361173dc005ca43768cf55
Matias Ignacio Perez Nauto <lit.io30303@gmail.com>
2024-05-17 18:12:57 -0400

    Finalizar v1

--- src/main/java/Formularios/MenuPrincipal.java
+++ src/main/java/Formularios/MenuPrincipal.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package Formularios;
 
 /**
@@ -15,6 +12,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
      */
     public MenuPrincipal() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
