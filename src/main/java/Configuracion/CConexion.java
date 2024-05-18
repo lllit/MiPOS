deb2732119d6cd4894b06f52e1ce70702fd3c5ce
Matías Ignacio Pérez Nauto <106929604+lllit@users.noreply.github.com>
2024-05-17 21:38:45 -0400

    Update CConexion.java

--- src/main/java/Configuracion/CConexion.java
+++ src/main/java/Configuracion/CConexion.java
@@ -15,7 +15,7 @@ public class CConexion {
     Connection conectar = null;
     
     String usuario="root";
-    String contraseña="Homero123";
+    String contraseña="contraseña";
     String bd="dbpos";
     String ip="localhost";
     String puerto="3306";
